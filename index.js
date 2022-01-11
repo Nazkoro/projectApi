e4fecf17eff8318f2fefba5df73dad3d4be6acc6
Nazar <rusetskiy2788@mail.ru>
2022-01-11 12:41:16 +0300

    create server and include dependency

--- index.js
+++
@@ -1,8 +0,0 @@
-const Modal = {
-	invalid,
-	closeModal,
-	handleSubmit,
-	change,
-	reset,
-};
-console.log('darov', 'kak dela');
---
+++ index.ts
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
