6ebd28d0e39214e63fe6c8f9efc924788503d4c8
Nazar <rusetskiy2788@mail.ru>
2022-03-23 18:26:00 +0300

    fix bug with admin panel

--- service/admin-service.ts
+++ service/admin-service.ts
@@ -1,6 +1,6 @@
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 import UserModel from "../models/User";
-import TokenModel from "../models/Token";
-import CommentModel from "../models/Comment";
 
 class AdminService {
   async getAllUsers() {
@@ -8,17 +8,44 @@ class AdminService {
     return users;
   }
 
-  async addUser(user) {
-    const newUser = new UserModel(user);
-    const saveuser = await newUser.save();
-    return saveuser;
+  async addUser(getUser) {
+    console.log("get user from admin", getUser);
+    // const newUser = new UserModel(getUser);
+    // const saveuser = await newUser.save();
+    // return saveuser;
+
+    // const { email, username, password } = getUser;
+
+    const { email, username, year, password, country, city, jobs, position } =
+      getUser;
+
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuidv4(); // v34fa-asfasf-142saf-sa-asf
+
+    const user = await UserModel.create({
+      email,
+      username,
+      password: hashPassword,
+      activationLink,
+      year,
+      country,
+      city,
+      jobs,
+      position,
+    });
+    console.log("saver user from admin ", user);
+    return user;
   }
 
   // update user
   async updUser(bodyOfPost) {
+    console.log("update from admin", bodyOfPost);
+    // eslint-disable-next-line no-param-reassign
+    bodyOfPost.password = await bcrypt.hash(bodyOfPost.password, 3);
     const user = await UserModel.findByIdAndUpdate(bodyOfPost._id, {
       $set: bodyOfPost,
     });
+    console.log("updated", user);
     // user.save()
     return user;
   }
